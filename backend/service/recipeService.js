9d3de797162e2fcdfa84b6baa339e4da03e0d2a2
gyuuuu <kai9702@gmail.com>
2020-09-29 03:18:52 +0900

    add : 조회된 레시피에서 부족한 재료수 반환기능

--- backend/service/recipeService.js
+++ backend/service/recipeService.js
@@ -1,3 +1,4 @@
+const refController = require('../controller/refController');
 const recipeDao = require('../dao/recipeDao');
 
 async function insertRecipe(recipeData){
@@ -46,11 +47,27 @@ async function insertRecipeforFood(recipeData){
 }
 
 async function getRecipeList(userId){
-    const recipeList =  recipeDao.selectRecipeListByUser(userId);
+    const recipeList =  await recipeDao.selectRecipeListByUser(userId);
     if(recipeList.length == 0){
         return -1;
     }
     else {
+        //console.log(typeof(recipeList[0].total))
+        let canMake = []; // 유저가 가진 재료로 만들 수 있는 레시피
+        let cantMake = []; // 몇개의 재료가 모자른 레시피
+        for(let i=0; i<recipeList.length; i++){
+            if(recipeList[i].total - recipeList[i].my_count == 0){
+                canMake.push(recipeList[i].recipe_name);
+            }
+            else {
+                const deficient_count = recipeList[i].total - recipeList[i].my_count;
+                cantMake.push({
+                    "recipe_name" : recipeList[i].recipe_name,
+                    "deficient_count" : deficient_count // 부족한 재료 수
+                });
+            }
+        }
+        
         return recipeList;
     }
 }
