48037abc9a3222654f499e588ac0633de3875995
KimByoungJun <65850608+KimByoungJun@users.noreply.github.com>
2020-10-09 17:33:33 +0900

    login 및 회원가입 구현

--- backend/routes/external_api.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const external_apiController = require('../controller/external_apiController');
-
-router.get('/api_to_db', external_apiController.api_to_db);
-
-module.exports = router;
---
+++ loginbackend/routes/loginroutes.js
@@ -0,0 +1,102 @@
+var mysql      = require('mysql');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'kimecr5p9253!!',
+  database : 'test'
+});
+connection.connect(function(err){
+
+  if(!err) {
+    console.log("Database is connected ... nn");
+  } else {
+    console.log("Error connecting database ... nn");
+  }
+});
+
+exports.register = async function(req,res){
+  const password = req.body.password;
+  const encryptedPassword = await bcrypt.hash(password, saltRounds)
+  var users={
+     "email":req.body.email,
+     "password":encryptedPassword
+  }
+  var email= req.body.email;
+  connection.query('SELECT * FROM users WHERE email = ?',[email],  function (error, results, fields) {
+    //Query 전송 실패
+    if (error) {
+      res.send({
+        "code":400,
+        "failed":"error ocurred"
+      })
+    }else{
+      //email이 이미 존재하는 경우
+      if(results.length >0){
+        res.send({
+          "code":421,
+          "failed":"Misdirected Request"
+        })
+      }
+      else{
+        connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
+          //Query 전송 실패
+          if (error) {
+            res.send({
+              "code":400,
+              "failed":"error ocurred"
+            })
+          } 
+          //회원가입 성공
+          else {
+            res.send({
+              "code":200,
+              "success":"user registered sucessfully"
+            });
+          }
+        });
+      }
+    }
+  }); 
+}
+
+exports.login = async function(req,res){
+  var email= req.body.email;
+  var password = req.body.password;
+  connection.query('SELECT * FROM users WHERE email = ?',[email], async function (error, results, fields) {
+    //Query 전송 실패
+    if (error) {
+      res.send({
+        "code":400,
+        "failed":"error ocurred"
+      })
+    }else{
+      //databse내 email 정보가 존재할 때
+      if(results.length >0){
+        const comparision = await bcrypt.compare(password, results[0].password)
+        //로그인 성공
+        if(comparision){
+            res.send({
+              "code":200,
+              "success":"login sucessfull"
+            })
+        }
+        //로그인 정보 불일치
+        else{
+          res.send({
+               "code":204,
+               "success":"Email and password does not match"
+          })
+        }
+      }
+      //database내 email정보가 존재하지 않을 때
+      else{
+        res.send({
+          "code":206,
+          "success":"Email does not exits"
+            });
+      }
+    }
+  });
+}
