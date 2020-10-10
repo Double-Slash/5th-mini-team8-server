512509f912157c6f7e95cb462d30f249fd8fe1dd
KimByoungJun <65850608+KimByoungJun@users.noreply.github.com>
2020-10-10 21:11:54 +0900

    로그인 회원가입 작동확인 완료

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+var login = require('./loginroutes');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.use('/ref', require('./ref'));
+
+router.post('/register',login.register);
+router.post('/login',login.login)
 
 // hidden url : 공공데이터 api로 부터 로컬 db로 데이터 저장하는 url 
 // 최초 한번만 호출해주면됨.
